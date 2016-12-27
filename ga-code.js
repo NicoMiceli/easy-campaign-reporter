1e9c6949784ac4b349c00c2548704c333b549edf
Nico Miceli <nicomiceli@gmail.com>
2016-12-27 12:02:24 -0500

    initial commit

---
+++ ga-code.js
@@ -0,0 +1,107 @@
+gapi.analytics.ready(function() {
+
+	var gaID = "";
+	var options = {
+		ids: ""
+	};
+	var startDate, endDate, gaOutput
+
+	gapi.analytics.auth.authorize({
+		container: 'embed-api-auth-container',
+		clientid: '877106660496-bvm53u3hi8jkb7nfmiait66dfgsmioku.apps.googleusercontent.com'
+	});
+
+	var tableHeaders = ["time", "total"]
+
+	var viewSelector = new gapi.analytics.ViewSelector({
+		container: 'view-selector-container'
+	});
+
+	// Render the view selector to the page.
+	viewSelector.execute();
+
+	function grabValue(id) {
+		var value = $("#" + id).val();
+		return value
+	}
+
+	// function grabDates(start,end) {
+	// 	var startInputValue = $("#"+start).val();
+	// 	var endInputValue = $("#"+end).val();
+	// 	if (startInputValue.length<1 && endInputValue.length<1){
+	// 		startDate = "30daysAgo";
+	// 		endDate = "yesterday";
+	// 	} else(
+	// 		startDate = startInputValue
+	// 		console.log(endInputValue)
+	// 	)
+	// }
+
+	/**
+	 * Create a new DataChart instance with the given query parameters
+	 * and Google chart options. It will be rendered inside an element
+	 * with the id "chart-container".
+	 */
+
+	viewSelector.on('change', function(ids) {
+		options.ids = ids;
+		gaID = ids;
+		console.log(options.ids);
+	});
+
+	//
+	// Button Click 
+	//
+	$("#get-data").click(function() {
+		var campaign = $("#campaign").val();
+		console.log("campaign = " + campaign)
+		grabDates("start","end");
+
+		var dataChart = new gapi.analytics.googleCharts.DataChart({
+			query: {
+				ids: gaID,
+				metrics: grabValue("metric"),
+				dimensions: grabValue("dim"),
+				'start-date': grabValue("start"),
+				'end-date': grabValue("end")
+			},
+			chart: {
+				container: 'chart-container',
+				type: 'LINE',
+				options: {
+					width: '100%'
+				}
+			}
+		});
+
+		var dataTable = new gapi.analytics.report.Data({
+			query: {
+				ids: gaID,
+				metrics: grabValue("metric"),
+				dimensions: grabValue("dim"),
+				'start-date': grabValue("start"),
+				'end-date': grabValue("end")
+			}
+		})
+
+		if (campaign.length > 0) {
+			dataTable.set({
+				query: {
+					filters: "ga:campaign==" + campaign
+				}
+			})
+			dataChart.set({
+				query: {
+					filters: "ga:campaign==" + campaign
+				}
+			})
+		}
+
+		console.log("button clicked")
+		dataTable.on('success', function(response) {
+			console.log(response);
+		})
+		dataTable.execute();
+		dataChart.execute();
+	});
+});
